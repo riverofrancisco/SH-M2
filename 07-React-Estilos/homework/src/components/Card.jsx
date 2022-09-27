d2306deb380a0eb067159a05054bcbd4eb5728af
riverofrancisco <francisco.rivero124@gmail.com>
2022-09-27 20:58:23 -0300

    React Styles 1st commit

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import styles from './Card.module.css';
 
-export default function Card(props) {
+export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return <div className={styles.box}>
+  <button className={styles.button} onClick = {onClose}>x</button>
+  <h3>{name}</h3>
+  <div className={styles.cityweather}>
+    <h5>Min</h5>
+    <p>{min}</p>
+  </div>
+  <div className={styles.cityweather}>
+    <h5>Max</h5>
+    <p>{max}</p>
+  </div>
+  <img className={styles.image} src={`http://openweathermap.org/img/wn/${img}@2x.png`} 
+  alt="icon"/>
+</div>
 };
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import styles from './SearchBar.module.css'
 
-export default function SearchBar(props) {
+export default function SearchBar({onSearch, }) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return <div className={styles.box}>
+  <input className={styles.input} type="text" placeholder='Ciudad...' />
+  <button className={styles.button} onClick={() => {onSearch('valor del input')}}>Agregar</button>
+</div>
 };
